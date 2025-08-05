24cdd36cc97fae5a7bf7af0ae5c29f90afeaf6fa
Agustin <agustinchumbita27@gmail.com>
2025-08-05 17:12:29 -0300

    Se realizo codigo de prueba para saber el comportamiento de la salida del sensor de etiqueta

--- src/etiquetadora.ino
+++ src/etiquetadora.ino
@@ -1,10 +1,15 @@
 #include <Arduino.h>
 
+int valorPin = 0;
 void setup() {
     // Initialize serial communication at 9600 baud rate
     Serial.begin(9600);
+    pinMode(8, INPUT);
 }
 
 void loop() {
+    valorPin = digitalRead(8);
+    Serial.print("Valor pin8: ");
+    Serial.println(valorPin);
     // Your main code here
 }
